aa95e0b082cffa1255278cf95ac732f86993476c
TarikBR <64391349+TarikBR@users.noreply.github.com>
2024-05-27 22:48:38 -0300

    initial commit

---
+++ src/main/java/pedrixzz/legendaryboots/LegendaryBoots.java
@@ -0,0 +1,10 @@
+package pedrixzz.legendaryboots;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LegendaryBoots implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
