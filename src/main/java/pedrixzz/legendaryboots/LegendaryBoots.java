fd7707b8c304e3dc41831fcd444487c11b95aac1
TarikBR <64391349+TarikBR@users.noreply.github.com>
2024-05-28 19:44:27 -0300

    feat: legendary boots and feather

--- src/main/java/pedrixzz/legendaryboots/LegendaryBoots.java
+++ src/main/java/pedrixzz/legendaryboots/LegendaryBoots.java
@@ -2,26 +2,53 @@ package pedrixzz.legendaryboots;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.component.DataComponentType;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.TextCodecs;
-
-import java.util.function.UnaryOperator;
 
 public class LegendaryBoots implements ModInitializer {
+
+    private int tick = 0;
 
     @Override
     public void onInitialize() {
         ServerTickEvents.END_SERVER_TICK.register(
                 (listener) -> {
-                    for(ServerPlayerEntity player: listener.getPlayerManager().getPlayerList()){
-                        //player.getMainHandStack();
+                    if(tick >= 20) {
+                        for (ServerPlayerEntity player : listener.getPlayerManager().getPlayerList()) {
+                            checkFeather(player);
+                            checkBoots(player);
+                        }
+                        tick = 0;
                     }
+                    tick++;
                 }
         );
+    }
+
+    private void checkFeather(ServerPlayerEntity player){
+        for(ItemStack itemStack: player.getHandItems()) {
+            if (Registries.ITEM.getId(itemStack.getItem()).toString().equals("minecraft:feather") && itemStack.get(DataComponentTypes.CUSTOM_DATA) != null) {
+                NbtCompound nbt = itemStack.get(DataComponentTypes.CUSTOM_DATA).copyNbt();
+                if (nbt.contains("legendary_feather")) {
+                    player.addStatusEffect(new StatusEffectInstance(StatusEffects.LEVITATION, 60, 0, false, false, false));
+                }
+            }
+        }
+    }
+
+    private void checkBoots(ServerPlayerEntity player){
+        for(ItemStack itemStack: player.getAllArmorItems()){
+            if(Registries.ITEM.getId(itemStack.getItem()).toString().equals("minecraft:diamond_boots") && itemStack.get(DataComponentTypes.CUSTOM_DATA) != null){
+                NbtCompound nbt = itemStack.get(DataComponentTypes.CUSTOM_DATA).copyNbt();
+                if(nbt.contains("legendary_boots")){
+                    player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 60, 3, false, false, false));
+                }
+            }
+        }
     }
 }
